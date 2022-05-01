74bd2928497004af1acab629b8300cb5698c09aa
ribbinpo <ribbinpo@gmail.com>
2022-05-02 01:10:54 +0700

    Update style information and scoll table in index/dashboard page

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,34 @@ import 'react-circular-progressbar/dist/styles.css';
 
 const Chart = dynamic(()=>import ("react-apexcharts"),{ ssr: false })
 
-const baba: NextPage = ({data,data2}:any) =>{
-    const percentage = 77
+type tableListProps = {
+  dataRank:{dataRank: {ranking:string,province:string,cases:string}[];}
+}
+
+const TableList = (props:tableListProps) =>{
+  const { dataRank } = props.dataRank;
+  return (
+    <>
+      <tr className="sticky top-0 flex w-full bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+        <th scope="row" className="w-1/3 p-4 font-medium text-gray-900 dark:text-white">100</th>
+        <td className="p-4 w-1/3">A</td>
+        <td className="p-4 w-1/3">B</td>
+      </tr>
+      {dataRank.map((res:{ranking:string,province:string,cases:string},idx:number)=>(
+      <tr key={idx} className="flex w-full bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+        <th scope="row" className="w-1/3 p-4 font-medium text-gray-900 dark:text-white">{res.ranking}</th>
+        <td className="p-4 w-1/3">{res.province}</td>
+        <td className="p-4 w-1/3">{res.cases}</td>
+      </tr>
+      ))}
+    </>
+  )
+}
+
+const baba: NextPage = ({data,data2,ranking}:any) =>{
+    
+    const percentage = data.accuracy
+
     const state = {
         options: {
           chart: {
@@ -35,87 +61,54 @@ const baba: NextPage = ({data,data2}:any) =>{
         <div className="flex flex-col h-screen"> 
             <div className="flex-grow px-2 pt-2">
                 <div className=" bg-zinc-50 rounded-lg h-screen text-center px-3">
-                    <div className="text-right text-xs pr-5 mb-2">Lastest Update: {data2.date}</div>
+                    <div className="text-right text-xs font-light pr-5 mb-2">Lastest Update: {data2.date}</div>
+                    {/* Row1 General Information */}
                     <div className="grid grid-cols-1 md:grid-cols-3 gap-3 mb-2">
                         <div className="bg-white p-5 rounded-lg">
-                          <div>TODAY CASE</div>
-                          <div>{data2.province.Phuket.new_case}</div>
+                          <div className="text-lg font-normal">TODAY CASE</div>
+                          <div className="text-6xl font-semibold">{data2.province.Phuket.new_case}</div>
                         </div>
                         <div className="bg-white p-5 rounded-lg">
-                          <div>TOMORROW CASE</div>
-                          <div>{data2.province.Phuket.total_case}</div>
+                          <div className=" text-lg font-normal">TOMORROW CASE</div>
+                          <div className="text-6xl font-semibold">{data.predictTomorrow}</div>
                         </div>
                         <div className="bg-white p-5 rounded-lg">
-                          <div>TOTAL CASE</div>
-                          <div>{data2.province.Phuket.total_case}</div>
+                          <div className="text-lg font-normal">TOTAL CASE</div>
+                          <div className="text-6xl font-semibold">{data2.province.Phuket.total_case}</div>
                         </div>
                     </div>
+                    {/* Row2 Accuracy+Ranking */}
                     <div className="grid grid-cols-1 md:grid-cols-3 gap-2 mb-2">
-                      <div className="bg-white col-span-1 rounded-lg">
-                          <div>ACCURACY</div>
+                      {/* Accuracy */}
+                      <div className="bg-white col-span-1 rounded-lg py-5">
+                          <div className="text-lg font-normal mb-2">ACCURACY</div>
                           <div className="d-flex flex justify-center">
                             <div className="w-1/3 md:w-2/5">
                               <CircularProgressbar value={percentage} text={`${percentage}%`} />
                             </div>
                           </div>
                       </div>
+                      {/* Ranking */}
                       <div className="bg-white col-span-2 rounded-lg">
-                            <div className="overflow-x-auto sm:rounded-lg rounded-lg shadow-sm pt-1">
-                              <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                                  <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                                      <tr>
-                                          <th scope="col" className="px-6 py-3">
-                                              Rank
-                                          </th>
-                                          <th scope="col" className="px-6 py-3">
-                                              Province
-                                          </th>
-                                          <th scope="col" className="px-6 py-3">
-                                              InfectedCases
-                                          </th>
-                                      </tr>
-                                  </thead>
-                                  <tbody>
-                                      <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
-                                            39
-                                        </th>
-                                        <td className="px-6 py-4">
-                                            Phuket
-                                        </td>
-                                        <td className="px-6 py-4">
-                                            1234
-                                        </td>
-                                      </tr>
-                                      <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
-                                            40
-                                        </th>
-                                        <td className="px-6 py-4">
-                                        Prachuap Khiri Khan
-                                        </td>
-                                        <td className="px-6 py-4">
-                                            93
-                                        </td>
-                                      </tr>
-                                      <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
-                                            41
-                                        </th>
-                                        <td className="px-6 py-4">
-                                        Prachuap Khiri Khan
-                                        </td>
-                                        <td className="px-6 py-4">
-                                            85
-                                        </td>
-                                      </tr>
-                                  </tbody>
-                              </table>
-                            </div>
+                        <div className="sm:rounded-lg rounded-lg shadow-sm pt-1">
+                        <table className="text-center w-full text-gray-500 dark:text-gray-400 rounded-lg">
+                          <thead className="flex w-full text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr className="flex w-full">
+                              <th className="p-4 w-1/3">Rank</th>
+                              <th className="p-4 w-1/3">Province</th>
+                              <th className="p-4 w-1/3">Infected Cases</th>
+                            </tr>
+                          </thead>
+                          <tbody className="bg-grey-light text-xs flex flex-col items-center justify-between overflow-y-scroll w-full max-h-52">
+                            <TableList dataRank={ranking} />
+                          </tbody>
+                        </table>
+                        </div>
                       </div>
                     </div>
+                    {/* Row3 COVID19 CASES GRAPH */}
                     <div className="p-5 bg-white rounded-lg">
-                        <div>COVID-19 CASES GRAPH</div>
+                        <div className="text-lg font-normal">COVID-19 INFECTED CASES GRAPH</div>
                         <hr className="mb-3" />
                         <Chart
                         options={state.options}
@@ -136,8 +129,15 @@ export const getServerSideProps:GetServerSideProps = async () =>{
     const data = await res.json()
     const res2 = await fetch("http://159.65.2.88/api/covid19-information")
     const data2 = await res2.json()
-    // console.log(data.data.date)
-    return {props:{data,data2}}  
+
+    let row = '{"dataRank":['
+    for(let i=0;i<data2.ranking.rank.length;i++){
+      row = row+'{"ranking": "'+(data2.ranking.rank[i]+1).toString()+'", "province": "'+data2.ranking.province[i]+'", "cases": "'+data2.ranking.cases[i].toString()+'"},'
+    }
+    row = row.substring(0,row.length-1)
+    row += ']}'
+    let ranking = JSON.parse(row)
+    return {props:{data,data2,ranking}}  
 }
 
 
